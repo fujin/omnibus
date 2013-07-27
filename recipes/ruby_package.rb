42215644587ae5604e92048727e9242bf1477de6
AJ Christensen <aj@junglist.io>
2013-07-27 16:31:40 +1200

    [ruby] nevermind debian or ruby from source, grab it from pkg for ubuntu for quicker omnibus vm bootstrap

---
+++ recipes/ruby_package.rb
@@ -0,0 +1,14 @@
+case node['platform']
+when 'ubuntu'
+  apt_repository 'brightboxuk' do
+    uri 'http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu'
+    distribution node['lsb']['codename']
+    components %w(main)
+    keyserver 'keyserver.ubuntu.com'
+    key 'C3173AA6'
+  end
+
+  %w(ruby1.9.3 ruby-switch).each do |ruby_package|
+    package ruby_package
+  end
+end
